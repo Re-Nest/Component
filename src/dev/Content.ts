c553b12c7ea74af975d0dd4ce10304f448b12d73
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2022-11-29 07:09:56 +0800

    Update

--- src/dev/Content.ts
+++ src/dev/Content.ts
@@ -1,17 +1,40 @@
-import {View, ViewWrapper} from "@renest/renest";
+import {Hook, Prop, required, State, View, ViewWrapper} from "@renest/renest";
 import {HStack, NavigationView, VStack} from "../component";
-import {Div} from "../component/Convert";
+import {Button, Div} from "../component/Convert";
 import Spacer from "../component/Other/Spacer";
+import {useNavigate} from "react-router-dom";
 
+
+class SubView1 extends View {
+    @Hook(useNavigate) navigate: any
+    Body = () =>
+        VStack(
+            Button("click1")
+                .onClick(() => {
+                    this.navigate(`/1`)
+                }),
+            Button("click2")
+                .onClick(() => {
+                    this.navigate(`/2`)
+                })
+        )
+}
+const Sub1 = ViewWrapper(SubView1)
+
+class SubView2 extends View {
+    @Prop n = required
+    Body = () =>
+        Div(this.n)
+}
+const Sub2 = ViewWrapper(SubView2)
 class Content extends View {
     Body = () =>
         HStack(
-            "hh",
-            Spacer(),
-            "ha",
-            Spacer(),
-            Spacer(),
-            "jj"
+            Sub1(),
+            NavigationView({
+                "/": () => Div("hh"),
+                ":": n => Sub2({n})
+            })
         ).width("200px")
 }
 
