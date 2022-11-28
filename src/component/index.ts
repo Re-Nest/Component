6bb214364e6bc3ed408aec1b0228a2391ee389ea
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2022-11-29 07:20:27 +0800

    Add list

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,8 +1,9 @@
 import HStack from "./Container/HStack"
 import VStack from "./Container/VStack"
 import ZStack from "./Container/ZStack"
+import List from "./Container/List"
 import NavigationView, {BrowserRouter} from "./Container/Navigation";
 import Spacer from "./Other/Spacer"
 
 
-export {HStack, VStack, ZStack, NavigationView, Spacer, BrowserRouter}
+export {HStack, VStack, ZStack, List, NavigationView, Spacer, BrowserRouter}
